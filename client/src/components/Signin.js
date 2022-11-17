19ac2a7b7f0680e1247f026fb20713eb46c45665
Pushkar Tiwari <pushkartiwari6@gmail.com>
2022-11-17 11:11:17 +0530

    Made the templete for SignIn and SignUp

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import {NavLink} from 'react-router-dom'
 export const Signin = () => {
   return (
-    <div>Signin
-
-<p>we are the mern developer!!</p>    
+    <div className='SignIn'>
+        <div className='Heading' >
+            <h2>Sign In</h2>
+        </div>
+        <div className='Input Fields'>
+            <form className='SignUp_form'>
+                <div className='SignUp_form_input'>
+                    <strong>Email:</strong>
+                    <input placeholder='Your Email' name='Email' autoComplete='off' type='email' id='email'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Password</strong>
+                    <input placeholder='Password' name='Password' autoComplete='off' type='password' id='password'/></div>
+                <div>
+                    <input style={{width: '100px'}} type="submit" name="submit" id='signup' value='Sign-in'/>
+                </div>
+            </form>
+                <div>
+                    
+                        <NavLink className="nav-link" to="/signup"><strong style={{margin: '10px', height: '30px'}} >Register</strong></NavLink>
+                        
+                   
+                </div>
+        </div>
 
 
     </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export const Signup = () => {
   return (
-    <div>Signup
+    <div className='SignUp'>
+        <div className='Heading' >
+            <h2>Sign up</h2>
+        </div>
+        <div className='Input Fields'>
+            <form className='SignUp_form'>
+                <div className='SignUp_form_input'>
+                    <strong>Name:</strong>
+                    <input placeholder='Name' name='Name' autoComplete='off' type='text' id='name'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Email:</strong>
+                    <input placeholder='Your Email' name='Email' autoComplete='off' type='email' id='email'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Phone</strong>
+                    <input placeholder='Your Phone' name='Phone' autoComplete='off' type='number' id='phone'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Profession</strong>
+                    <input placeholder='Your Profession' name='work' autoComplete='off' type='text' id='work'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Password</strong>
+                    <input placeholder='Password' name='Password' autoComplete='off' type='password' id='password'/></div>
+                <div className='SignUp_form_input'>
+                    <strong>Confirm Password</strong>
+                    <input placeholder='Confirm Password' name='CPassword' autoComplete='off' type='password' id='cpassword'/></div>
+                <div>
+                    <input type="submit" name="submit" id='signup' value='register'/>
+                </div>
+            </form>
+                <div>
+                    <>Already have an account?</><NavLink className="nav-link" to="/signin"><strong style={{margin: '10px', height: '30px'}} >SignIn</strong></NavLink>
 
-<p>we are the mern developer!!</p>    
+                </div>
+        </div>
+  
 
     </div>
   )
